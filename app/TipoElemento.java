c1793717f36b9bc0b153965d6b74d25840d5f5a4
Murillo-pedroso <murillomonteirop@gmail.com>
2021-05-10 16:28:45 -0300

    alterei a classe de enum para class

--- app/TipoElemento.java
+++ app/TipoElemento.java
@@ -1,22 +1,9 @@
-public enum TipoElemento {
-    AGUA(1,"AGUA"),
-    FOGO(2,"FOGO"),
-    TERRA(3,"TERRA"),
-    LUTADOR(4,"LUTADOR"),
-    SOMBRIO(5,"SOMBRIO"),
-    FANTASMA(6,"FANTASMA"),
-    DRAGAO(7,"DRAGAO"),
-    FADA(8,"FADA"),
-    PLANTA(9,"PLANTA"),
-    PSIQUICO(10,"PSIQUICO"),
-    NORMAL(11,"NORMAL"),
-    PEDRA(12,"PEDRA"),
-    NULO(13,"");
+public class TipoElemento {
+  
+    private int valor;
+    private String tipo;
 
-    public int valor;
-    public String tipo;
-
-    private TipoElemento(int valor,String tipo) {
+    TipoElemento(int valor,String tipo) {
         this.valor = valor;
         this.tipo = tipo;
     }
@@ -25,7 +12,7 @@ public enum TipoElemento {
         return 0;
     }
     public String getTipo(){
-        return this.tipo;
+        return tipo;
     }
 
 
